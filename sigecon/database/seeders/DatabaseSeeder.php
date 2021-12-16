31c95dd5f491e78282e2d73c6ccefcd98ddb9e8a
shenriquess <saulo.souza@aluno.ufop.edu.br>
2021-12-15 23:11:45 -0300

    Login configuration

--- sigecon/database/seeders/DatabaseSeeder.php
+++ sigecon/database/seeders/DatabaseSeeder.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +13,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call('UsersTableSeeder');
     }
+}
+
+class UsersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create([
+            'name'=>'Saulo Henrique',
+            'email'=>'sktsaulo@hotmail.com',
+            'password'=>bcrypt('admin'),
+        ]);
+    }
+
 }
